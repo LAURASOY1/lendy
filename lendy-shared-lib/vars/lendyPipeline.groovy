5a22db5a3642927d3608ef37d76878d593b8be41
LAURA_SOY <100204298+LAURASOY1@users.noreply.github.com>
2025-07-18 13:14:57 +0300

    Update lendyPipeline.groovy

--- lendy-shared-lib/vars/lendyPipeline.groovy
+++ lendy-shared-lib/vars/lendyPipeline.groovy
@@ -1,27 +1,19 @@
 // lendy-shared-lib/vars/lendyPipeline.groovy
 def call(Map config = [:]) {
-    // Remove the pipeline block - just define the stages
+    // Remove the pipeline block and steps{} wrappers
     stage('Checkout') {
-        steps {
-            checkout scm
-        }
+        checkout scm
     }
     stage('Build') {
-        steps {
-            sh "docker-compose -f ${config.dockerComposeFile ?: 'docker-compose.yml'} build"
-        }
+        sh "docker-compose -f ${config.dockerComposeFile ?: 'docker-compose.yml'} build"
     }
     stage('Test') {
-        steps {
-            sh config.testCommand ?: 'python manage.py test'
-        }
+        sh config.testCommand ?: 'python manage.py test'
     }
     stage('Deploy') {
         when {
             branch 'main'
         }
-        steps {
-            sh 'docker-compose up -d'
-        }
+        sh 'docker-compose up -d'
     }
 }
